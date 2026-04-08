b1a7b1fcbe0d1cea0256f3e89f74aa9e5f819312
Shivang1109 <shivangpathak11@gmail.com>
2026-04-08 09:59:12 +0530

    commit Changes

--- Leetcode/twoDarray.java
+++ Leetcode/twoDarray.java
@@ -3,7 +3,7 @@ package Leetcode;
 public class twoDarray {
     static void transpose(int[][] arr){
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
+            for(int j=i;j<arr.length;j++){
                 int temp=arr[i][j];
                 arr[i][j]=arr[j][i];
                 arr[j][i]=temp;
